e4b4fe5dfa0bc6688c63e255967ad174956ebb40
yassineassian <126581186+yassineassian@users.noreply.github.com>
2023-04-04 16:20:57 +0200

    Add files via upload

---
+++ oc-install-ide-quiz-master/src/main/java/com/openclassrooms/ideinstall/QuizExample.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.ideinstall;
+
+import ij.IJ;
+import ij.ImagePlus;
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class QuizExample {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final int compteARebours = 5;
+        System.out.println("Compte à rebours !");
+        for (int i = compteARebours; i >=0; i--) {
+            System.out.println("--> " + i);
+            Thread.sleep(1000);
+        }
+        final Path cheminImage = cheminFichier(choixSecret());
+        final ImagePlus imp = IJ.openImage(cheminImage.toString());
+        imp.show();
+        Files.delete(cheminImage);
+        Files.delete(cheminImage.getParent());
+    }
+
+    public static String choixSecret() {
+        return new String(Base64.decodeBase64("bGFwaW4="));
+    }
+
+    public static Path cheminFichier(String image) throws IOException {
+        final String nomImage = image + ".jpg";
+        final Path tmpDir = Files.createTempDirectory(image);
+        final Path cheminFichier = tmpDir.resolve(nomImage);
+        Files.copy(QuizExample.class.getClassLoader().getResourceAsStream(nomImage), cheminFichier);
+        return cheminFichier;
+    }
+}
